911b3e559dc85afbbafcc9cf7eff6d1e2bd50210
王杭 <2381886606@qq.com>
2022-07-13 15:05:25 +0800

    第三周第三题：分发饼干

---
+++ src/week3/FindContentChildren.java
@@ -0,0 +1,19 @@
+package week3;
+
+import java.util.Arrays;
+import java.util.spi.CurrencyNameProvider;
+
+public class FindContentChildren {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+
+        for (int i = 0, j = 0; i < g.length && j < s.length; i++, j++) {
+            while (j < s.length && g[i] > s[j]) j++;
+            if (j < s.length) count++;
+        }
+
+        return count;
+    }
+}
