eab316dda4e42a3a0b7d3fde31568c74f433ead3
王杭 <2381886606@qq.com>
2022-07-17 22:12:52 +0800

    第三周第五题：盛水最多的容器

---
+++ src/week3/AddBinary.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class AddBinary {
+    public String addBinary(String a, String b) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 2;
+        System.out.println(i ^ j);
+        i = 6;
+        j = 7;
+        System.out.println(i ^ j);
+    }
+}
---
+++ src/week3/MaxArea.java
@@ -0,0 +1,18 @@
+package week3;
+
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(area, ans);
+            if (height[l] <= height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return ans;
+    }
+}
