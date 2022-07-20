42d5d46b2aa301fea56bcc0f7621330a29d922a4
王杭 <2381886606@qq.com>
2022-07-20 13:07:47 +0800

    第四周 第三题: 遍历二叉树

---
+++ src/Tree/Traversal.java
@@ -0,0 +1,81 @@
+package Tree;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;}
+}
+
+/*
+    递归
+ */
+public class Traversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<Integer>();
+        preOrder(root, res);
+        return res;
+    }
+
+    public void preOrder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preOrder(root.left, res);
+        preOrder(root.right, res);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<Integer>();
+        if (root == null) return res;
+
+        TreeNode node = root;
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
+        }
+        return res;
+    }
+}
+
+/*
+    迭代
+ */
+class Traversal2 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<Integer>();
+        if (root == null) return res;
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                res.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        return res;
+    }
+}
