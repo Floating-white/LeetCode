ff97622a8a721b44437ba6d3e424b1ffea6628af
王杭 <2381886606@qq.com>
2022-07-25 11:20:36 +0800

    路径总和

--- src/Tree/MaxDepth.java
+++ src/Tree/MaxDepth.java
@@ -1,6 +1,9 @@
 package Tree;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaxDepth {
     /*
      *   深度优先搜索
@@ -37,6 +40,37 @@ public class MaxDepth {
         root.left = right;
         root.right = left;
         return root;
+    }
+
+
+    /*
+    *   路经总和
+    * */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        Queue<TreeNode> queNodes = new LinkedList<>();
+        Queue<Integer> queVal = new LinkedList<>();
+        queNodes.offer(root);
+        queVal.offer(root.val);
+
+        while (!queNodes.isEmpty()) {
+            TreeNode now = queNodes.poll();
+            int temp = queVal.poll();
+            if (now.left == null && now.right == null) {
+                if (temp == targetSum) return true;
+                continue;
+            }
+            if (now.left != null) {
+                queNodes.offer(now.left);
+                queVal.offer(now.left.val + temp);
+            }
+
+            if (now.right != null) {
+                queNodes.offer(now.right);
+                queVal.offer(now.right.val + temp);
+            }
+        }
+        return false;
     }
 }
 
